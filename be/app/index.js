7efd35408e6dccb4f0e7d52a884667d0a79d6bad
matus <bartko.mat@gmail.com>
2018-01-06 19:37:12 +0100

    aws renamed to AWS

--- be/app/index.js
+++ be/app/index.js
@@ -1,10 +1,10 @@
 'use strict';
 
-const aws = require('aws-sdk');
+const AWS = require('aws-sdk');
 
 exports.handler = (event, context, callback) => {
     console.log("teest");
-    console.log(aws);
+    console.log(AWS);
 
     let ddb = new AWS.DynamoDB({apiVersion: '2012-10-08'}); //TODO wat is this 2012-10-08
 
@@ -24,7 +24,7 @@ exports.handler = (event, context, callback) => {
             };
         } else {
             let resp = {
-                statusCode: '500',
+                statusCode: '200',
                 body: data.Item
             };
             console.log("Success", data.Item);
