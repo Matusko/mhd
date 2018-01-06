61eea6c47d8a8119b67375315681d31f6f267549
matus <bartko.mat@gmail.com>
2018-01-06 19:26:37 +0100

    access to dynamodb added

--- be/app/index.js
+++ be/app/index.js
@@ -1,12 +1,35 @@
 'use strict';
 
-var aws = require('aws-sdk');
+const aws = require('aws-sdk');
 
 exports.handler = (event, context, callback) => {
     console.log("teest");
     console.log(aws);
-    callback(null, {
-        statusCode: '200',
-        body: 'The time in Los bratislava is: ',
+
+    let ddb = new AWS.DynamoDB({apiVersion: '2012-10-08'}); //TODO wat is this 2012-10-08
+
+    let params = {
+        TableName: 'schedule',
+        Key: {
+            'firstName' : "first item",
+        }
+    };
+
+    ddb.getItem(params, function(err, data) {
+        if (err) {
+            console.log("Error", err);
+            let resp = {
+                statusCode: '500',
+                body: 'error '
+            };
+        } else {
+            let resp = {
+                statusCode: '500',
+                body: data.Item
+            };
+            console.log("Success", data.Item);
+        }
     });
+
+    callback(null, resp);
 };
